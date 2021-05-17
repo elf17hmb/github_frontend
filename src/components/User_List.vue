52fb7c642b1a0bbbd66fef467443498c5bbd04e8
Eldiar Flügel <eldiar.fluegel@gmail.com>
2021-05-17 16:43:34 +0200

    implemented better looking way to add new users in onboarding with modal

--- src/components/User_List.vue
+++ src/components/User_List.vue
@@ -1,38 +1,109 @@
 <template>
-    <div class="border  mt-5">
-        <div class="row m-2 gy-3 gx-5">
-            <div v-if="loading">
-            <span class="spinner-border spinner-border-sm"></span>
-            Loading...
-            </div>
-            <div v-else v-for="user in users" v-bind:key="user.id" class="col-6 ">
-                <div class="row rounded border">
-                    <div class="col">
-                        <img id="pfp" v-bind:src="user.avatar_url" class=" rounded-circle rounded float-start"/>
-                    </div>
-                    <div class="col text-start">
-                        <span>{{user.login}}</span>
-                    </div>
-                </div>
-            </div>
+  <div class="border mt-5">
+    <div class="row m-2 gy-3 gx-5">
+      <div v-if="loading">
+        <span class="spinner-border spinner-border-sm"></span>
+        Loading...
+      </div>
+      <div v-else v-for="user in users" v-bind:key="user.id" class="col-6">
+        <div class="row rounded border">
+          <div class="col">
+            <img id="pfp" v-bind:src="user.avatar_url" class="img-fluid rounded-circle rounded float-start" />
+          </div>
+          <div class="col text-start">
+            <span>{{ user.login }}</span>
+          </div>
         </div>
+      </div>
+      <div class="col">
+        <button class="btn btn-light h-100 w-100" data-bs-toggle="modal" data-bs-target="#onboardingModal">
+          <h1 class="align-self-center">+</h1>
+        </button>
+      </div>
     </div>
+  </div>
+  <!-- Modal -->
+  <div class="modal fade" id="onboardingModal" tabindex="-1" aria-labelledby="onboardingModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1>Onboarding</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-12">
+              <textarea v-model="ghnames" name="gh-handles" id="handlestext" cols="30" rows="10"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button @click="parseNames" class="btn btn-primary" data-bs-dismiss="modal">
+            <div v-if="loading">
+              <span class="spinner-border spinner-border-sm"></span>
+              Loading...
+            </div>
+            <div v-else>Suchen</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import API_Service from '../services/API'
+
 export default {
-  props: {
-    loading: {
-        type: Boolean,
-        default: true,
-    },
-    users: Array,
+  data() {
+    return {
+      ghnames: '',
+      users: [],
+      loading: false
+    }
   },
-};
+  methods: {
+    lookUpUsers(names) {
+      let promises = []
+      names.forEach((name) => {
+        promises.push(
+          API_Service.getUser(name)
+            .then((response) => {
+              this.users.push(response.data)
+            })
+            .catch((error) => {
+              console.log('User not found: ', error)
+            })
+        )
+      })
+      Promise.all(promises).then(() => {
+        console.log('Loading users finished!')
+        this.loading = false
+      })
+    },
+    parseNames() {
+      this.loading = true
+      //   this.users = []
+      let str = this.ghnames
+      let names = str.split(/\r?\n/)
+      this.ghnames = ''
+
+      names = names
+        .map((name) => name.trim())
+        .filter((name) => {
+          return name != null && name != ''
+        }) //trimming names and deleting empty names
+        names = names.filter((name) => {
+            return !this.users.includes(name) //don't include duplicates
+        })
+      this.lookUpUsers(names)
+    }
+  }
+}
 </script>
 
 <style scoped>
-#pfp{
-    width: 5rem;
+#pfp {
+  width: 4rem;
 }
 </style>
--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -1,21 +1,8 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12">
-        <h1>Onboarding</h1>
-        <textarea v-model="ghnames" name="gh-handles" id="handlestext" cols="30" rows="10"></textarea>
-      </div>
-      <div class="col-12">
-        <button @click="parseNames" class="btn btn-primary">
-          <div v-if="loading">
-            <span class="spinner-border spinner-border-sm"></span>
-            Loading...
-          </div>
-          <div v-else>Suchen</div>
-        </button>
-      </div>
       <div class="col">
-        <UserList :loading="loading" :users="users" />
+        <UserList />
       </div>
       <div class="col-12 mt-3">
         <h3>Gefundene Nutzer in die Organisation einladen</h3>
@@ -45,9 +32,6 @@ import User_List from '../components/User_List.vue'
 export default {
   data() {
     return {
-      ghnames: '',
-      users: [],
-      loading: false,
       orgs: [],
       selected: ''
     }
@@ -64,34 +48,6 @@ export default {
   },
 
   methods: {
-    parseNames() {
-      this.loading = true
-      this.users = []
-      let str = this.ghnames
-      let names = str.split(/\r?\n/)
-      let promises = []
-
-      names = names
-        .map((name) => name.trim())
-        .filter((name) => {
-          return name != null && name != ''
-        }) //trimming names and deleting empty names
-      names.forEach((name) => {
-        promises.push(
-          API_Service.getUser(name)
-            .then((response) => {
-              this.users.push(response.data)
-            })
-            .catch((error) => {
-              console.log('User not found: ', error)
-            })
-        )
-      })
-      Promise.all(promises).then(() => {
-        console.log('Loading users finished!')
-        this.loading = false
-      })
-    },
     inviteUsers() {
       this.users.forEach((user) => {
         API_Service.inviteUser(this.selected, user.id).then((response) => {
